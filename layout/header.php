aa2c0fcd183992ef18a7fb758a9a3fd70761e74c
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-08-12 19:01:54 +0430

    header and updateHeader pages are added...

---
+++ layout/header.php
@@ -0,0 +1,51 @@
+<header>  
+<style type="text/css">
+#ad{
+    margin-left: 5px;
+}
+#te{
+    margin-left: 5px;
+}
+#st{
+    margin-left: 5px;
+}
+</style> 
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="navigation">
+                <div class="container">                 
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="navbar-brand">
+                            <a href="../webpanel/index.php"><h1><span>Eng</span>-Com</h1></a>
+                        </div>
+                    </div>
+                    
+                    <div class="navbar-collapse collapse">                          
+                        <div class="menu">
+                            <ul class="nav nav-tabs" role="tablist">
+                                <li role="presentation"><a href="../webpanel/index.php" class="active">home</a></li>
+                                <li role="presentation"><a href="../webpanel/about.php">About Us</a></li>
+                                <li role="presentation"><a href="../webpanel/services.php">Services</a></li>                             
+                                <li role="presentation"><a href="../webpanel/portfolio.php">Publish</a></li>
+                                <li role="presentation"><a href="../webpanel/blog.php">Info</a></li>
+                                <li role="presentation"><a href="../webpanel/contact.php">Contact</a></li>
+                                <li role="presentation"><a href="../webpanel/login.php" style="color:blue;background-color:pink" class="dropdown-toggle" data-toggle="dropdown">Login</a>
+                                    <ul class="dropdown-menu" style="background-color:pink">
+                                        <li id="ad" style="color:black;background-color:silver"><a href="../adminpanel/login/alogin.php">As Admin</a></li>
+                                        <li id="te" style="color:blue;background-color:plum"><a href="../adminpanel/login/tlogin.php">As Teacher</a></li>
+                                        <li id="st" style="color:white;background-color:gray"><a href="../adminpanel/login/slogin.php">As Student</a></li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>                      
+                </div>
+            </div>  
+        </nav>      
+    </header>
---
+++ layout/updateHeader.php
@@ -0,0 +1,64 @@
+  <header class="header dark-bg">
+      <div class="toggle-nav">
+        <div class="icon-reorder tooltips" data-original-title="Toggle Navigation" data-placement="bottom"><i class="icon_menu"></i></div>
+      </div>
+
+      <!--logo start-->
+      <a href="../../adminpanel/A_index.php" class="logo">Eng <span class="lite">Com</span></a>
+      <!--logo end-->
+
+      <div class="nav search-row" id="top_menu">
+        <!--  search form start -->
+        <ul class="nav top-menu">
+          <li>
+            <form class="navbar-form">
+              <input class="form-control" placeholder="Search" type="text">
+            </form>
+          </li>
+        </ul>
+        <!--  search form end -->
+      </div>
+
+      <div class="top-nav notification-row">
+        <!-- notificatoin dropdown start-->
+        <ul class="nav pull-right top-menu">
+         
+          <!-- user login dropdown start-->
+          <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <span class="profile-ava">
+                              <img src="../../public/img/mudasir.jpg">
+                            </span>
+                            <span class="username">Wahidullah Mudasir</span>
+                            <b class="caret"></b>
+                        </a>
+            <ul class="dropdown-menu extended logout">
+              <div class="log-arrow-up"></div>
+              <li class="eborder-top">
+                <a href="A_profile"><i class="icon_profile"></i> My Profile</a>
+              </li>
+              <li>
+                <a href="../../adminpanel/A_login.php"><i class="icon_key_alt"></i> Log Out</a>
+              </li>
+            </ul>
+          </li>
+          <!-- user login dropdown end -->
+        </ul>
+        <!-- notificatoin dropdown end-->
+      </div>
+    </header>
+ 
+ <aside>
+      <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu">
+          <li class="active">
+            <a class="" href="../../adminpanel/A_index.php">
+                          <i class="icon_house_alt"></i>
+                          <span>Home</span>
+                      </a>
+          </li>
+        </ul>
+        <!-- sidebar menu end-->
+      </div>
+    </aside>
