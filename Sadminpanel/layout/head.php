efc2cb0460a50008bba608772c2011f243d4ed0f
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-02 19:28:20 +0430

    head and header pages added..

---
+++ Sadminpanel/layout/head.php
@@ -0,0 +1,26 @@
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>EngCom</title>
+
+    <!-- Core CSS - Include with every page -->
+    <link href="../design/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../design/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <!-- Page-Level Plugin CSS - Dashboard -->
+    <link href="../design/css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
+    <link href="../design/css/plugins/timeline/timeline.css" rel="stylesheet">
+
+    <!-- SB Admin CSS - Include with every page -->
+    <link href="../design/css/sb-admin.css" rel="stylesheet">
+
+    <!-- Page-Level Plugin CSS - Buttons -->
+    <link href="../design/css/plugins/social-buttons/social-buttons.css" rel="stylesheet">
+
+    <!-- Page-Level Plugin CSS - Tables -->
+    <link href="../design/css/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+
+</head>
+
