5b3ea9484572661b8e0294273180d72a4be9a7d7
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-04 12:15:09 +0430

    qitToother and stepToother pages added..

---
+++ Sadminpanel/Side_bar/stepToother.php
@@ -0,0 +1,311 @@
+
+<style type="text/css">
+
+#side-menu{
+    overflow: scroll;
+    overflow: vertical;
+    width: 250px;
+    height: 600px;
+}
+</style>
+            
+
+            <div class="navbar-default navbar-static-side" role="navigation">
+                <div class="sidebar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Computer_Section <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> CIT <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="../CIT_Package/window_seven.php"> Windows-7</a>
+                                </li>
+                                <li>
+                                    <a href="../CIT_Package/window_eight.php"> Windows-8</a>
+                                </li>
+                                <li>
+                                    <a href="../CIT_Package/window_ten.php"> Windows-10</a>
+                                </li>
+                                <li>
+                                    <a href="../CIT_Package/typing_pashto_dari.php"> Typing(Pashto/Dari)</a>
+                                </li>
+                                <li>
+                                    <a href="../CIT_Package/MS.word.php"> Ms.Word</a>
+                                </li>
+                                <li>
+                                    <a href="../CIT_Package/Ms.excel.php">Ms.Excel</a>
+                                </li>
+                                <li>
+                                    <a href="../CIT_Package/MS.powerpoint.php"> Ms.PowerPoint</a>
+                                </li>
+                                <li>
+                                    <a href="../CIT_Package/internet_collection.php"> Internet-Collection</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                         <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> DIT <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="../DIT_Package/MSaccess.php"> Ms.Access(Advanced)</a>
+                                </li>
+                                <li>
+                                    <a href="../DIT_Package/AdobePhotoshopCC.php"> Adobe Photoshop CC</a>
+                                </li>
+                                <li>
+                                    <a href="../DIT_Package/CoralDrawX7.php"> Coral Draw X7</a>
+                                </li>
+                                <li>
+                                    <a href="../DIT_Package/UleadVideoStudio.php"> Ulead Video Studio</a>
+                                </li>
+                                <li>
+                                    <a href="../DIT_Package/ComputerHardware.php"> Computer Hardware</a>
+                                </li>
+                                <li>
+                                    <a href="../DIT_Package/AdvancedNetworking.php"> Advanced Networking</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                         <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> QIT <span class="fa arrow"></span></a>
+                           <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="../QIT_Package/window_seven_qit.php"> Windows-7</a>
+                                </li>
+                                <li>
+                                    <a href="../QIT_Package/window_eight_qit.php"> Windows-8</a>
+                                </li>
+                                <li>
+                                    <a href="../QIT_Package/window_ten_qit.php"> Windows-10</a>
+                                </li>
+                                <li>
+                                    <a href="../QIT_Package/typing_pashto_dari_qit.php"> Typing(Pashto/Dari)</a>
+                                </li>
+                                <li>
+                                    <a href="../QIT_Package/MS.word_qit.php"> Ms.Word</a>
+                                </li>
+                                <li>
+                                    <a href="../QIT_Package/Ms.excel_qit.php">Ms.Excel</a>
+                                </li>
+                                <li>
+                                    <a href="../QIT_Package/MS.powerpoint_qit.php"> Ms.PowerPoint</a>
+                                </li>
+                                <li>
+                                    <a href="../QIT_Package/internet_collection_qit.php"> Internet-Collection</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                         <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Graphics <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="../Graphics_Packages/adobe_illustrator_cc.php"> Adobe Illustrator CC</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/adobe_after_effect_cc.php"> Adobe After Effect CC</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/coral_video_studio_pro_x7.php"> Coral Video Studio Pro X7</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/adobe_premiere_cc.php"> Adobe Premiere CC</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/aurara_3danimation_maker.php"> Aurara 3D Animation Maker</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/auto_play_media_studio8.php">Auto Play Media Studio 8</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/plotagon2016.php"> Plotagon 2016</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/muvizo.php"> Muvizu</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/real_draw_pro2018.php"> Real Draw Pro 2018</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/inkscape.php"> Inkscape</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/adobe_muse_cc2018.php"> Adobe Muse CC 2018</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/cool_edit.php"> Cool Edit</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/adobe_audition_cc2018.php"> Adobe Audition CC 2018</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/adobe_flash_cc2016.php"> Adobe Flush CC 2016</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/audio_video_editing.php"> Audio Video Editing</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/3ds_max.php"> 3Ds Max</a>
+                                </li>
+                                <li>
+                                    <a href="../Graphics_Packages/quick_book.php"> Quick Book</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                         <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> BigApp <span class="fa arrow"></span></a>
+                             <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="../BigApp_Package/html_pad2018.php"> HTML Pad 2018</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/css5.php"> CSS 5</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/c.php"> C</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/c++.php"> C++</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/java.php"> Java</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/csharp.php"> C#</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/dreamweaver_cc2018.php"> DreamWeaver CC 2018</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/php.php"> PHP</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/vb_studio.php"> Visual Basic</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/apk_studio_pro2018.php"> APK Studio Pro 2018</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/Android_studio.php"> Android Studio</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/cmd.php"> CMD</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/javascript.php"> JavaScript</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/jquery.php"> Jquery</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/linux.php"> Linux</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/camera_editing.php"> Camera Editing</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/autocad2017.php"> Auto Cad 2017</a>
+                                </li>
+                                 <li>
+                                    <a href="../BigApp_Package/sql_server2018.php"> SQL Server 2018</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/sketch_up.php"> Sketch Up</a>
+                                </li>
+                                <li>
+                                    <a href="../BigApp_Package/SolidWorks2018.php"> Solid Work 2018</a>
+                                </li>
+                                 <li>
+                                    <a href="../BigApp_Package/ibm_spss2014.php"> IBM Spss 2018</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> English_Section <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                               <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Step Classess <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="step-1.php"> Step-1</a>
+                                </li>
+                                <li>
+                                    <a href="step-2.php"> Step-2</a>
+                                </li>
+                                <li>
+                                    <a href="step-3.php"> Step-3</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Base Classes <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="../Base_Classes/base-1.php"> Base-1</a>
+                                </li>
+                                <li>
+                                    <a href="../Base_Classes/base-2.php"> Base-2</a>
+                                </li>
+                                <li>
+                                    <a href="../Base_Classes/junior-1.php"> Junior-1</a>
+                                </li>
+                                <li>
+                                    <a href="../Base_Classes/junior-2.php"> Junior-2</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Advanced Classes <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="../Advanced_Classes/Senior-1.php"> Senior-1</a>
+                                </li>
+                                <li>
+                                    <a href="../Advanced_Classes/Senior-2.php"> Senior-2</a>
+                                </li>
+                                <li>
+                                    <a href="../Advanced_Classes/Advanced-1.php"> Advanced-1</a>
+                                <li>
+                                    <a href="../Advanced_Classes/Advanced-2.php"> Advanced-2</a>
+                                </li>
+                                 <li>
+                                    <a href="../Advanced_Classes/Graduation.php"> Graduation</a>
+                                </li>
+                                <li>
+                                    <a href="../Advanced_Classes/Conversation.php"> Conversation</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li> 
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                    <!-- /#side-menu -->
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
