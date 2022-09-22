07075c9fd2613143a36d93fd6300dbda9619187d
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-22 09:42:39 +0330

    footer and header pages added..

---
+++ Swebpanel/demo/layout/footer.blade.php
@@ -0,0 +1,67 @@
+<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="widget">
+						<h5 class="widgetheading">Nam libero tempore</h5>						
+						<p>soluta nobis est eligendi optio cumque nihil impedit quo minus id
+						excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
+						
+						</p>						
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="widget">
+						<h5 class="widgetheading">Pages</h5>
+						<ul class="link-list">
+							<li><a href="#">Press release</a></li>
+							<li><a href="#">Terms and conditions</a></li>
+							<li><a href="#">Privacy policy</a></li>
+							<li><a href="#">Career center</a></li>
+							<li><a href="#">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="widget">
+						<h5 class="widgetheading">Latest posts</h5>
+						<ul class="link-list">
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Pellentesque et pulvinar enim</a></li>
+							<li><a href="#">Natus error sit voluptatem </a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+				
+		<div class="sub-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<ul class="social-network">
+							<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus fa-1x"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-lg-12">
+						<div class="copyright">
+							<p>
+								<span>&copy;Resi 2014 All right reserved. By </span><a href="http://bootstraptaste.com" target="_blank">Bootstraptaste</a>
+							</p>
+                            <!-- 
+                                All links in the footer should remain intact. 
+                                Licenseing information is available at: http://bootstraptaste.com/license/
+                                You can buy this theme without footer links online at: http://bootstraptaste.com/buy/?theme=Resi
+                            -->
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</footer>
---
+++ Swebpanel/demo/layout/header.blade.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+	<title>Mytitle</title>
+
+ <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resi Bootstrap Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/responsive-slider.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="css/style.css" rel="stylesheet">	
+   
+
+</head>
+<body>
+<header>
+		<div class="container">
+			<div class="row">
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<div class="navbar-brand">
+								<a href="{!!asset('index')!!}"><h1>Resi</h1></a>
+							</div>
+						</div>
+						<div class="menu">
+							<ul class="nav nav-tabs" role="tablist">
+								<li role="presentation" class="active"><a href="{!!asset('index')!!}">Home</a></li>
+								<li role="presentation"><a href="{!!asset('feature')!!}">Feature</a></li>
+								<li role="presentation"><a href="{!!asset('blog')!!}">Blog</a></li>
+								<li role="presentation"><a href="{!!asset('portfolio')!!}">Portfolio</a></li>
+								<li role="presentation"><a href="{!!asset('contact')!!}">Contact</a></li>						
+							</ul>
+						</div>
+					</div>			
+				</nav>
+			</div>
+		</div>
+	</header>
+</body>
+</html>
+
+
+
